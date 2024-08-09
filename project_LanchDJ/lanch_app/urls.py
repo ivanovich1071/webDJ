e6043e9a1fafd23a4d880c11c76283296c1650b7
ivanovich1071 <163851033+ivanovich1071@users.noreply.github.com>
2024-08-09 18:17:37 +0300

    new sett09

--- project_LanchDJ/lanch_app/urls.py
+++ project_LanchDJ/lanch_app/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('order/', views.order, name='order'),
     path('info/', views.info, name='info'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- project_LanchDJ/project_LanchDJ/settings.py
+++ project_LanchDJ/project_LanchDJ/settings.py
@@ -35,11 +35,10 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # Необходим для аутентификации
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'project_LanchDJ.urls'
 
 TEMPLATES = [
