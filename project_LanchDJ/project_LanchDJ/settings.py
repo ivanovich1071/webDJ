57d683364b4c2d3f7c442fb861eeb2289fd21750
ivanovich1071 <163851033+ivanovich1071@users.noreply.github.com>
2024-08-09 16:23:31 +0300

    add new urls

--- project_LanchDJ/project_LanchDJ/settings.py
+++ project_LanchDJ/project_LanchDJ/settings.py
@@ -106,10 +106,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'lanch_app/static']
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # project_LanchDJ/settings.py
