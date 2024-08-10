dcc41ff9ff0ce803489a2594fff1015914b19535
ivanovich1071 <163851033+ivanovich1071@users.noreply.github.com>
2024-08-10 16:43:04 +0300

    add admin

--- project_LanchDJ/lanch_app/admin.py
+++ project_LanchDJ/lanch_app/admin.py
@@ -8,6 +8,17 @@ admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Comment)
 admin.site.register(Post)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'image_preview', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('price',)
+    ordering = ('name',)
 
+    # Добавляем метод для отображения превью изображения
+    def image_preview(self, obj):
+        return obj.image_url and obj.image_url.url
 
+    image_preview.short_description = 'Превью изображения'
+    image_preview.allow_tags = True
 
